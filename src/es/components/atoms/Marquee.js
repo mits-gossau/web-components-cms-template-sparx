aa65d029da34b87435235cc3a3bbf0051210a948
Silvan Strübi <silvan.struebi@mgb.ch>
2021-08-16 13:24:23 +0200

    marquee and cleaning

--- src/es/components/atoms/Marquee.js
+++ src/es/components/atoms/Marquee.js
@@ -10,10 +10,15 @@ import { Shadow } from '../web-components-cms-template/src/es/components/prototy
  * @class Sample
  * @type {CustomElementConstructor}
  * @attribute {
-
+ *  {number} [animation-duration=20]
  * }
  * @css {
-
+ *  var(--background-color, red);
+    var(--color, white);
+    var(--font-size, 45px);
+    var(--grid-area, footer);
+    var(--padding, 0.672em);
+    var(--font-size-mobile, var(--font-size, 1rem));
  * }
  */
 export default class Marquee extends Shadow() {
@@ -37,19 +42,28 @@ export default class Marquee extends Shadow() {
    */
   renderCSS () {
     this.css = /* css */`
-
-      :host > * {
-        animation:marquee ${this.getAttribute('animation-duration') || 20}s linear infinite;
+      :host {
+        background-color: var(--background-color, red);
+        color: var(--color, white);
+        font-size: var(--font-size, 45px);
+        grid-area: var(--grid-area, footer);
+        padding: var(--padding, 0.672em);
+        white-space: nowrap;
       }
-
-
-
+      :host > * {
+        animation: marquee ${this.getAttribute('animation-duration') || 20}s linear infinite;
+      }
       @keyframes marquee{
         from{
           transform:translateX(101%);
         }
         to{
           transform:translateX(-101%)
+        }
+      }
+      @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
+        :host {
+          font-size: var(--font-size-mobile, var(--font-size, 1rem));
         }
       }
     `
