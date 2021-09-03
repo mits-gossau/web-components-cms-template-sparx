ba351e318a6254a06c29941b489f5b4038686c98
Silvan Strübi <silvan.struebi@mgb.ch>
2021-09-03 10:49:43 +0200

    fixed and linted

--- src/es/components/organisms/Badge.js
+++ src/es/components/organisms/Badge.js
@@ -1,6 +1,8 @@
 // @ts-check
 import Style from './Style.js'
 
+/* global self */
+
 /**
  * Defines a badge wrapper for content - Wrap Heading and CallForIdeas Components
  * Example at: /src/es/components/pages/kosmos.html
@@ -16,29 +18,13 @@ import Style from './Style.js'
  */
 export default class Badge extends Style {
   /**
-   * evaluates if a render is necessary
-   *
-   * @return {boolean}
-   */
-  shouldComponentRenderHTML () {
-    return false
-  }
-
-  /**
    * renders the o-highlight-list css
    *
    * @return {void}
    */
   renderCSS () {
     super.renderCSS()
-    const bodyCss = this.css.replace(/\s>\smain/g, '')
-    this.css = ''
     this.css = /* css */`
-      ${bodyCss}
-      :host  {
-        ${Array.from(this.attributes).reduce((acc, attribute) => `${acc}${attribute.name}: ${attribute.value};--${attribute.name}: ${attribute.value};`, '')}
-      }
-      
       :host > div {
         display:flex;
         flex-direction: row;
@@ -46,11 +32,9 @@ export default class Badge extends Style {
         justify-content: center;
         width: var(--content-width, 100%);
       }
-
       :host > div > * {
-        margin:var(--margin, 2rem);
+        margin:var(--margin, 2em);
       }
-
       @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
         :host > div {
           flex-direction: column;
