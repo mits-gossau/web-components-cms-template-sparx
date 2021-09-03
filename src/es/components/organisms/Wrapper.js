32863729a6646bf27f3c87f49a26ffe4f08275d0
Silvan Strübi <silvan.struebi@mgb.ch>
2021-09-03 09:33:53 +0200

    wrapper

--- src/es/components/organisms/Wrapper.js
+++ src/es/components/organisms/Wrapper.js
@@ -1,12 +1,17 @@
 // @ts-check
 import { Shadow } from '../web-components-cms-template/src/es/components/prototypes/Shadow.js'
 
+/* global self */
+
 /**
  * As an organism, this component shall hold molecules and/or atoms
  *
  * @export
  * @class Wrapper
  * @type {CustomElementConstructor}
+ * @attribute {
+ *  {has} first (tells the first item to be big, else its the last)
+ * }
  * @css {
  *  --margin [20px]
  *  --min-width [370px]
@@ -16,6 +21,7 @@ import { Shadow } from '../web-components-cms-template/src/es/components/prototy
 export default class Wrapper extends Shadow() {
   connectedCallback () {
     if (this.shouldComponentRenderCSS()) this.renderCSS()
+    if (this.shouldComponentRenderHTML()) this.renderHTML()
   }
 
   /**
@@ -28,12 +34,20 @@ export default class Wrapper extends Shadow() {
   }
 
   /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldComponentRenderHTML () {
+    return !this.root.querySelector('section')
+  }
+
+  /**
    * renders the o-teaser-wrapper css
    *
    * @return {void}
    */
   renderCSS () {
-    const teaserAmount = this.root.querySelectorAll('m-teaser').length
     this.css = /* css */`
       :host {
         --carousel-content-width: 100%;
@@ -42,28 +56,42 @@ export default class Wrapper extends Shadow() {
         --picture-width-mobile: 100%;
         --picture-width: 100%;
         --spacing: 0.5rem;
-        display: var(--display, flex);
-        flex-wrap: var(--flex-wrap, wrap);
+        display: block;
+      }
+      :host > section {
+        display: flex;
+        flex-wrap: wrap;
         justify-content: var(--justify-content, space-between);
       }
-      :host > * {
+      :host > section > * {
         width: calc(50% - var(--spacing));
-      }
-      :host > *:first-child {
-        margin-bottom: 3.5%;
-        width: 100%;
       }
       @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
         :host {
           --carousel-content-width-mobile: 100%;
+        }
+        :host > section {
           display: block;
         }
-        :host > *:not(style) {
+        :host > section > * {
           display: block;
           width: 100%;
           margin: var(--content-spacing-mobile, var(--content-spacing, unset)) auto;
         }
       }
     `
+  }
+
+  /**
+   * renders the html
+   *
+   * @return {void}
+   */
+  renderHTML () {
+    const section = document.createElement('section')
+    Array.from(this.root.children).forEach(node => {
+      if (node.tagName !== 'STYLE') section.appendChild(node)
+    })
+    this.html = section
   }
 }
